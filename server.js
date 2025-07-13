d12cbfbf13ef71dc5e071f24cc83e8cb8b2db7a7
mohd <generalassemblyse@gmail.com>
2025-07-13 16:09:32 +0300

    made ejs css files & pages

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3001;
+const path = require('path');
 
+
+// MiddLEWARE
+// to connect public folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req,res) => {
-  res.send('working!')
+  res.render('index.ejs');
 })
 
-app.listen(port, () => {
+app.listen(port), () => {
   console.log("SERVER IS WORKING");
-})
+}
